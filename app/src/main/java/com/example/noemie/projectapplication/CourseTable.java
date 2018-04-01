a8528ffb75ef063e7cf1672d1520f85f7ec3d0b3
NoemieLG <noemie.lyonnard@gmail.com>
2018-04-01 12:59:08 +0200

    Affichage de toutes les courses ajoutées dans la SQLite Bdd.

---
+++ app/src/main/java/com/example/noemie/projectapplication/CourseTable.java
@@ -0,0 +1,77 @@
+package com.example.noemie.projectapplication;
+
+/**
+ * Created by Noemie on 30/03/2018.
+ */
+
+public class CourseTable {
+    private long id;
+    private String nom;
+    private String  date;
+    private String lat;
+    private String lon;
+    private String time;
+    private String com;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String name) {
+        this.nom = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLon() {
+        return lon;
+    }
+
+    public void setLon(String lon) {
+        this.lon = lon;
+    }
+
+    public String getTemps() {
+        return time;
+    }
+
+    public void setTemps(String tps) {
+        this.time = tps;
+    }
+
+    public String getCom() {
+        return com;
+    }
+
+    public void setCom(String com) {
+        this.com = com;
+    }
+
+    /*Méthode utilisée par ArrayAdapter dans la listView*/
+    public String toString(){
+        return ("Nom: " + nom + "Date: " + date + "Latitude: " + lat + "Longitude : "
+                + lon + "Temps: " + time + "Commentaire: " + com);
+    }
+}
